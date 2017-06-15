275ffc9b334def3b8660c08c116f5357f1653564
eallanjoseph123 <eallanjoseph@yahoo.com>
2017-06-15 16:30:51 +1200

    deleted files

--- car-app/src/main/java/com/online/rental/car/service/CarApp.java
+++
@@ -1,5 +0,0 @@
-package com.online.rental.car.service;
-
-public interface CarApp {
-
-}
--- car-app/src/main/java/com/online/rental/car/service/HomeService.java
+++
@@ -1,7 +0,0 @@
-package com.online.rental.car.service;
-
-import com.online.rental.car.model.CarAppResponse;
-
-public interface HomeService<response extends CarApp> {
-	public CarAppResponse<response> getAvailableCars();
-}
